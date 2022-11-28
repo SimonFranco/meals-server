7910c482d76d6ab171c29b425f26e5fe45540888
Simon Franco <simonfranco0@gmail.com>
2022-11-27 22:34:18 -0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const userRoutes = require('./userRoutes')
 const app = express();
+const cors = require('cors')
 require('./db')
+app.use(cors())
 app.use(express.urlencoded({extended: true}));
-// app.use(express.json())
+app.use(express.json())
 app.use(userRoutes);
+
 app.listen(5000,()=>{
     console.log('Server running at port 5000');
 })
